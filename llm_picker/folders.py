468595bb36230be6adbae9bd4b585661b2a395d9
Zeus Chiu <zeuscsc@gmail.com>
2023-07-24 10:42:02 +0800

    rename cache folder for response to llm_response_caches

--- llm_picker/folders.py
+++ llm_picker/folders.py
@@ -7,8 +7,8 @@ def reset_root_folder(path:str):
         LLM_CONVERSATION_STREAM_CACHE_FOLDER,\
         LLM_FOLDER
     ROOT_FOLDER=path
-    CACHES_FOLDER = f"{ROOT_FOLDER}caches"
-    LLM_RESPONSE_CACHE_FOLDER = f"{CACHES_FOLDER}\llm_caches"
+    CACHES_FOLDER = f"{ROOT_FOLDER}llm_caches"
+    LLM_RESPONSE_CACHE_FOLDER = f"{CACHES_FOLDER}\llm_response_caches"
     LLM_STREAM_RESPONSE_CACHE_FOLDER = f"{CACHES_FOLDER}\llm_stream_response_cache"
     LLM_CONVERSATION_STREAM_CACHE_FOLDER = f"{CACHES_FOLDER}\llm_conversation_stream_cache"
     LLM_FOLDER = f"{ROOT_FOLDER}llm"
