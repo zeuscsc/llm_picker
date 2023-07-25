fea06619c2137917776a8244f3c3227593fe6c9d
Zeus Chiu <zeuscsc@gmail.com>
2023-07-25 14:37:10 +0800

    cache foler path from other direction (seprator)

--- llm_picker/folders.py
+++ llm_picker/folders.py
@@ -8,9 +8,9 @@ def reset_root_folder(path:str):
         LLM_FOLDER
     ROOT_FOLDER=path
     CACHES_FOLDER = f"{ROOT_FOLDER}llm_caches"
-    LLM_RESPONSE_CACHE_FOLDER = f"{CACHES_FOLDER}\llm_response_cache"
-    LLM_STREAM_RESPONSE_CACHE_FOLDER = f"{CACHES_FOLDER}\llm_stream_response_cache"
-    LLM_CONVERSATION_STREAM_CACHE_FOLDER = f"{CACHES_FOLDER}\llm_conversation_stream_cache"
+    LLM_RESPONSE_CACHE_FOLDER = f"{CACHES_FOLDER}/llm_response_cache"
+    LLM_STREAM_RESPONSE_CACHE_FOLDER = f"{CACHES_FOLDER}/llm_stream_response_cache"
+    LLM_CONVERSATION_STREAM_CACHE_FOLDER = f"{CACHES_FOLDER}/llm_conversation_stream_cache"
     LLM_FOLDER = f"{ROOT_FOLDER}llm"
 
 reset_root_folder("./")
