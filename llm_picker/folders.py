1d23bc7a783c5f75fdabfc59e5b7328e3943d056
Zeus Chiu <zeuscsc@gmail.com>
2023-07-26 17:35:05 +0800

    os.path.join

--- llm_picker/folders.py
+++ llm_picker/folders.py
@@ -1,4 +1,4 @@
-
+import os
 def reset_root_folder(path:str):
     global ROOT_FOLDER,\
         CACHES_FOLDER,\
@@ -8,9 +8,9 @@ def reset_root_folder(path:str):
         LLM_FOLDER
     ROOT_FOLDER=path
     CACHES_FOLDER = f"{ROOT_FOLDER}llm_caches"
-    LLM_RESPONSE_CACHE_FOLDER = f"{CACHES_FOLDER}/llm_response_cache"
-    LLM_STREAM_RESPONSE_CACHE_FOLDER = f"{CACHES_FOLDER}/llm_stream_response_cache"
-    LLM_CONVERSATION_STREAM_CACHE_FOLDER = f"{CACHES_FOLDER}/llm_conversation_stream_cache"
+    LLM_RESPONSE_CACHE_FOLDER = os.path.join(CACHES_FOLDER,"llm_response_cache")
+    LLM_STREAM_RESPONSE_CACHE_FOLDER = os.path.join(CACHES_FOLDER,"llm_stream_response_cache")
+    LLM_CONVERSATION_STREAM_CACHE_FOLDER = os.path.join(CACHES_FOLDER,"llm_conversation_stream_cache")
     LLM_FOLDER = f"{ROOT_FOLDER}llm"
 
 reset_root_folder("./")
